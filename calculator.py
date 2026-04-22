54235991ba097c0d9d2719258e5babd3075d9a6b
hankcch2003 <hank.cch.2003@gmail.com>
2026-04-22 23:33:05 +0800

    更新 calculator.py：加入次方功能

--- calculator.py
+++ calculator.py
@@ -10,7 +10,11 @@ def multiply(a, b):
 def divide(a, b):
     return a // b
 
+def power(a, b):
+    return a ** b
+
 print(f"3 + 5 = {add(3, 5)}")
 print(f"10 - 4 = {subtract(10, 4)}")
 print(f"6 * 7 = {multiply(6, 7)}")
 print(f"20 // 4 = {divide(20, 4)}")
+print(f"2 ^ 10 = {power(2, 10)}")
