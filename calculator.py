174e1fd1466b423e82944150f8d95135281bca67
hankcch2003 <hank.cch.2003@gmail.com>
2026-04-22 23:03:28 +0800

    更新 calculator.py：加入乘法與除法功能

--- calculator.py
+++ calculator.py
@@ -4,5 +4,13 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
 print(f"3 + 5 = {add(3, 5)}")
 print(f"10 - 4 = {subtract(10, 4)}")
+print(f"6 * 7 = {multiply(6, 7)}")
+print(f"20 / 4 = {divide(10, 4)}")
