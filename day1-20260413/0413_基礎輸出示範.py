b8352c2615ac64213e32add05208907bc4068702
hankcch2003 <105833935+hankcch2003@users.noreply.github.com>
2026-04-27 22:23:02 +0800

    Add files via upload

---
+++ day1-20260413/0413_基礎輸出示範.py
@@ -0,0 +1,12 @@
+print("\n第一個程式：")
+print("Hello from my-first-python!")
+
+print("\n第二個程式：")
+print("Hello, Python!")
+print("我正在學習 Python 程式語言")
+print("1 + 1 =", 1 + 1)
+
+print("\n第三個程式：")
+print("姓名：陳承翰")
+print("日期：2026/04/13")
+print("Python 好好玩！")
